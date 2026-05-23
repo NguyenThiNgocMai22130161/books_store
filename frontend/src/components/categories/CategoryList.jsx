0de19acf5bc542d15a656bae9add109090e30676
hung <22130092@st.hcmuaf.edu.vn>
2026-05-23 17:06:14 +0700

    chỉnh sửa btn

--- frontend/src/components/categories/CategoryList.jsx
+++ frontend/src/components/categories/CategoryList.jsx
@@ -79,13 +79,15 @@ const CategoryList = () => {
                 {categories.length} danh mục | Tổ chức và quản lý các danh mục sách
               </p>
             </div>
-            <Link to="/categories/add" className="btn btn-primary">
+            <Link to="/categories/add" className="btn btn-primary"style={{ maxWidth: '40%' }}
+                                                                  >
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                 <line x1="12" y1="5" x2="12" y2="19"/>
                 <line x1="5" y1="12" x2="19" y2="12"/>
               </svg>
               Thêm Danh Mục Mới
             </Link>
+
           </div>
         </div>
 
