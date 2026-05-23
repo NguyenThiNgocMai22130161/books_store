944ee7b8b5fd3e8532346e12628310387c3e7931
Mai Nguyễn <146100178+NguyenThiNgocMai22130161@users.noreply.github.com>
2026-05-23 17:53:38 +0700

    Revert "thêm vào yêu thích và xóa khỏi yêu thích"

--- frontend/src/components/books/index.js
+++ frontend/src/components/books/index.js
@@ -2,4 +2,3 @@
 export { default as BookList } from './BookList';
 export { default as BookDetail } from './BookDetail';
 export { default as BookForm } from './BookForm';
-export { default as Wishlist } from './Wishlist';
